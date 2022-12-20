a2f0bfe511bb1555962702da57ecfaa7b1862d74
Anjaehyun <lvet0330@gmail.com>
2022-12-20 21:00:15 +0900

    # 객체 통해서 queryparameter 받기

--- hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,5 +1,6 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,20 +10,20 @@ import java.util.Map;
 public class GetApiController {
 
     @GetMapping(path = "/hello") //http://localhost:8080/api/get/hello
-    public String Hello(){
+    public String Hello() {
         return "get Hello";
     }
 
     @RequestMapping(path = "/hi", method = RequestMethod.GET)
     //get / post / put / delete 다들어감 method 지정해주면 get만
-    public String hi(){
+    public String hi() {
         return "hi";
     }
 
     // http://localhost:8080/api/get/path-variable/{name}
     @GetMapping("/path-variable/{name}")
-    public String pathVariable(@PathVariable(name = "name") String pathName){
-        System.out.println("PathVariable : " + pathName);;
+    public String pathVariable(@PathVariable(name = "name") String pathName) {
+        System.out.println("PathVariable : " + pathName);
         return pathName;
 
     }
@@ -35,19 +36,40 @@ public class GetApiController {
     // &ie = UTF-8
     // ?key = values&key2=value2
 
-    //http://localhost:8080/api/get/query-param?user=steve&email=steve@gmail.com&age=30
+    //http://localhost:8080/api/get/query-param?name=steve&email=steve@gmail.com&age=30
     @GetMapping(path = "query-param")
-    public String queryParam(@RequestParam Map<String, String> queryParam){
+    public String queryParam(@RequestParam Map<String, String> queryParam) {
         //데이터가 키밸류 키밸류 라서 맵
-        StringBuilder sb =new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-        queryParam.entrySet().forEach(entry ->{
-                System.out.println(entry.getKey());
-                System.out.println(entry.getValue());
-                System.out.println("\n");
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
 
-                sb.append(entry.getKey()+ " = " +entry.getValue()+"\n");
-                });
+            sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        });
         return sb.toString();
+    }
+
+    @GetMapping(path = "query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ) {
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+        return name + " " + email + " " + age;
+    }
+
+    @GetMapping(path = "query-param03")
+    public String queryParam(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
     }
 }
