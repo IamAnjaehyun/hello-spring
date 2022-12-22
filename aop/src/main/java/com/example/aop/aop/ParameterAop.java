3398302b053d441de28f01a90b82be8ce3de2679
IamJaehyun <lvet0330@gmail.com>
2022-12-22 21:53:32 +0900

    # AOP Aspect(1)
    aop 로 로그 남길 수 있음
    Aspect : 흩어진 관심사를 모듈화 한 것. 주로 부가기능을 모듈화함.
    PointCut : JointPoint의 상세한 스펙을 정의한 것. 'A란 메서드의 진입 시점에 호출할 것'과 같이 더욱 구체적으로 Advice가 실행될 지점을 정할 수 있음

---
+++ aop/src/main/java/com/example/aop/aop/ParameterAop.java
@@ -0,0 +1,40 @@
+package com.example.aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+//aop 로 로그 남길 수 있음
+@Aspect
+@Component
+public class ParameterAop {
+    @Pointcut("execution(* com.example.aop.controller..*.*(..))")
+    private void cut(){}
+
+    @Before("cut()")
+    public void before(JoinPoint joinPoint){
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println("메서드 이름 출력");
+        System.out.println(method.getName()); // 메서드 이름 출력
+
+
+        Object[] args = joinPoint.getArgs(); //매개변수들의 배열
+        for(Object obj : args){
+            System.out.println("method : "+obj.getClass().getSimpleName()); //타입 가져옴
+            System.out.println("value : "+ obj);
+        }
+        System.out.println("위에꺼 before ----------------------");
+
+    }
+
+    @AfterReturning(value = "cut()", returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj){
+        System.out.println("after returning ----------------------");
+        System.out.println("return obj : " + returnObj);
+    }
+
+}
