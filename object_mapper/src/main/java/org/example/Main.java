567a643fb54662e0333a7475f9361e557bdad027
Anjaehyun <lvet0330@gmail.com>
2022-12-23 18:51:38 +0900

    # Object Mapper 더 알아보기
    JSON 파싱하기 + 값 바꾸기

---
+++ object_mapper/src/main/java/org/example/Main.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.example.dto.Car;
+import org.example.dto.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+        System.out.println("main");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User user = new User();
+        user.setName("홍길동");
+        user.setAge(10);
+
+        Car car1 = new Car();
+        car1.setName("K5");
+        car1.setCarNumber("11가 1111");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("Q5");
+        car2.setCarNumber("22가 2222");
+        car2.setType("SUV");
+
+        List<Car> carList = Arrays.asList(car1, car2);
+        user.setCars(carList);
+
+//        System.out.println(user);
+
+        String json = objectMapper.writeValueAsString(user);
+        System.out.println(json);
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String _name = jsonNode.get("name").asText();
+        int _age = jsonNode.get("age").asInt();
+        System.out.println("name : " + _name);
+        System.out.println("age : " + _age);
+
+        JsonNode cars = jsonNode.get("cars");
+        ArrayNode arrayNode = (ArrayNode) cars;
+
+        List<Car> _cars = objectMapper.convertValue(arrayNode, new TypeReference<List<Car>>() {}); //파싱
+        System.out.println(_cars);
+
+        //json 안에 있는 값 바꾸기 jsonNode의 값을 바꾸기
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        objectNode.put("name", "steve"); //이름 바꾸기
+        objectNode.put("age", 20); //나이 바꾸기
+
+        System.out.println(objectNode.toPrettyString()); //예쁘게 출력해서 프리티스트링
+    }
+}
