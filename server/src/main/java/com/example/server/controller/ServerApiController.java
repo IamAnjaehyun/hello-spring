f1b4a1598ed50645e028772d6244d32d0a70dfbc
Anjaehyun <lvet0330@gmail.com>
2023-01-05 00:00:03 +0900

    # ServerToServer - Naver API 지역검색 연동
    encode(Charset.forName("UTF-8")) 사용해서 검색어 UTF-8로 변환
    헤더에 아이디 비밀번호 넣어야함
            RequestEntity<Void> req = RequestEntity//헤더 get으로 넣을거라 <String> 아니고 <Void>
                    .get(uri)
                    .header("X-Naver-Client-Id", "아이디")
                    .header("X-Naver-Client-Secret", "비번")
                    .build();

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,12 +3,56 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    //https://openapi.naver.com/v1/search/local.json
+    //?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver() {
+
+        String query = "갈비집";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "중국집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        log.info("uri : {}",uri);
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity//헤더 get으로 넣을거라 <String> 아니고 <Void>
+                .get(uri)
+                .header("X-Naver-Client-Id", "x4PUf50MM8jfKX2zuPba")
+                .header("X-Naver-Client-Secret", "u9uWsnlkof")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange( req, String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
@@ -21,11 +65,11 @@ public class ServerApiController {
     @PostMapping("/user/{userId}/name/{userName}")
     public Req<User> post(
 //                     HttpEntity<String> entity,
-                     @RequestBody Req<User> user,
-                     @PathVariable int userId,
-                     @PathVariable String userName,
-                     @RequestHeader("x-authorization") String authorization,
-                     @RequestHeader("custom-header") String customHeader
+            @RequestBody Req<User> user,
+            @PathVariable int userId,
+            @PathVariable String userName,
+            @RequestHeader("x-authorization") String authorization,
+            @RequestHeader("custom-header") String customHeader
     ) {
 //        log.info("req : {}",entity.getBody());
         log.info("userId : {}, userName : {}", userId, userName);
