6d37a3cc71f2eb65942d25882c5f56b3a6ccdfd2
Anjaehyun <lvet0330@gmail.com>
2023-01-05 16:01:31 +0900

    # JUnit으로 테스트 하기 - JUnit
    Java 기반의 단위 테스트를 위한 프레임워크
    Annotation 기반으로 테스트를 지원하며, Assert를 통하여, (예상, 실제) 를 통해 검증

---
+++ claculator/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello JUnit");
+
+        Calculator calculator = new Calculator(new KrwCalculator());
+
+        System.out.println("KRWCALCULATOR");
+        System.out.println(calculator.sum(10,10));
+
+        System.out.println("=============");
+
+
+    }
+}
